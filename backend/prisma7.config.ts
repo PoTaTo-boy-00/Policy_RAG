f671190f7416f98740cefa8df7b2ac714ae90ea9
PoTaTo-boy-00 <ayanangshudutta1@gmail.com>
2026-09-15 17:16:33 +0530

    added env fiox and docker ix

--- backend/prisma7.config.ts
+++ backend/prisma7.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "src/prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"] ||"",
+    url: process.env["DATABASE_URL"] ||"postgresql://postgres:postgres@localhost:5433/myapp?schema=public",
   },
 });
